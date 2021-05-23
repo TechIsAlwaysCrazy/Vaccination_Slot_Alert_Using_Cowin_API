619b197eef3a07d26f18b9ae57d1b5e2e393c60e
RahulMR42 <rahulmasquee@gmail.com>
2021-05-23 10:16:22 +0530

    Update cowin_scripts.py

--- cowin_scripts.py
+++ cowin_scripts.py
@@ -6,7 +6,7 @@ import argparse
 def action(date,dist_id):
   try:
     url = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={dist_id}&date={date}'
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers,timeout = 5)
     output=response.json()
     date_ref=datetime.datetime.now()
     for centers in output['centers']:
@@ -18,6 +18,7 @@ def action(date,dist_id):
         if int(min_age_limit) == 18:
           if int(available_capacity) > 0:
             winsound.Beep(400, 2000) #it will make a beep
+            winsound.Beep(200, 2000)
           print(f'{date_ref}=> Range From :{date},distric_id:{dist_id} Center:{center_name} ,Age:{min_age_limit},Capacity:{available_capacity},Address:{center_address}')
   except Exception as e:
     print("Exception " + str(e))
@@ -28,9 +29,10 @@ headers = {
 date= datetime.date.today().strftime("%d-%m-%Y")
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--id',type=str, help='Dsitrict ID', required=True)
+parser.add_argument('-d','--date',type=str,help='Custom start date',default=date)
 args = parser.parse_args()
 for id in args.id.split(","):
-  action(date,int(id))
+  action(args.date,int(id))
 
 
 
